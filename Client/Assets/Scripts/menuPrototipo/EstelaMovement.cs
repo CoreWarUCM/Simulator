8986257f959a69c6fe943a72837371b6ed1ea4ca
Stiven <starias@ucm.es>
2022-03-10 12:34:30 +0100

    ADD: Efecto de estrella fugaz en los títulos para darle más color

--- Client/Assets/Scripts/menuPrototipo/EstelaMovement.cs
+++ Client/Assets/Scripts/menuPrototipo/EstelaMovement.cs
@@ -7,50 +7,75 @@ using UnityEngine.UI;
 public class EstelaMovement : MonoBehaviour
 {
     [SerializeField] private Image starImage;
-    [SerializeField] private Transform[] corners;
-    [SerializeField] private float speed = 500.0F;
+    [SerializeField] private float speed = 200.0F;
     [SerializeField] private float beginTime = 0.0f;
-    [SerializeField] Transform startPosition;
-    [SerializeField] float alpha;
-    [SerializeField] Color startColor;
 
-    private int index = 0;
-    private Vector3 startMarker;
-    private Vector3 endMarker;
+    // Para invocar a ChangePath
+    private bool changed = false;
+    // Index del array de esquinas
+    private int indCorner = 0;
+    // Index del array de colores
+    private int indColor = 0;
+    // Momento en el que empieza el movimiento
     private float startTime;
+    // Distancia del camino
     private float journeyLength;
-
-    private void Start()
+    // Valor alpha del color del objeto
+    private float alpha = 1.0f;
+    // Punto inicial del movimiento
+    private Vector3 startMarker;
+    // Punto final del movimiento
+    private Vector3 endMarker;
+    // Array de esquinas
+    private Transform [] corners;
+    // Color inicial del objeto
+    private Color startColor;
+    // Array de transición de colores
+    private Color [] colors;
+    
+    public void Init(float a, Transform [] corns, Color [] cols)
     {
+        // Tiempo
         startTime = Time.time;
-        startColor.a = alpha;
+        // Colores
+        colors = cols;
+        startColor = colors[0];
+        startColor.a = alpha = a;
         starImage.color = startColor;
-        startMarker = startPosition.position;
-        endMarker = corners[index + 1].position;
+        // Posiciones
+        corners = corns;
+        startMarker = transform.position;
+        endMarker = corners[indCorner + 1].position;
         journeyLength = Vector3.Distance(startMarker, endMarker);
         
         InvokeRepeating(nameof(AnimationLoop), beginTime, Time.deltaTime);
     }
-
+    
     private void AnimationLoop()
     {
         float distCovered = (Time.time - startTime) * speed;
         float fracJourney = distCovered / journeyLength;
         transform.position = Vector3.Lerp(startMarker, endMarker, fracJourney);
         
-        if (fracJourney < 1.0f) return;
-        ChangePath();
+        if (fracJourney < 1.0f || changed) return;
+        changed = true;
+        Invoke(nameof(ChangePath), 0.0f);
     }
 
     private void ChangePath()
     {
-        index++;
+        // Reset del tiempo
         startTime = Time.time;
-        if (index == corners.Length) index = 0;
-        startMarker = corners[index].position;
-        endMarker = index + 1 == corners.Length ? corners[0].position : corners[index + 1].position;
+        changed = false;
+        // Cambio de posiciones
+        indCorner = indCorner == corners.Length - 1 ? 0 : indCorner + 1;
+        startMarker = corners[indCorner].position;
+        endMarker = indCorner + 1 == corners.Length ? corners[0].position : corners[indCorner + 1].position;
         journeyLength = Vector3.Distance(startMarker, endMarker);
-        //Color c = new Color(Random.Range(0, 1.0f), Random.Range(0, 1.0f), Random.Range(0, 1.0f), alpha);
-        //starImage.color = c;
+        // Cambio de color
+        indColor = indColor == colors.Length - 1 ? 0 : indColor + 1;
+        Color c = colors[indColor];
+        c.a = alpha;
+        starImage.color = c;
     }
 }
